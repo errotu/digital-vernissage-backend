7515953b4704d1b693fc2554d4abf82cd340df93
soenke <me@eknoes.de>
2017-07-13 19:08:43 +0200

    Fixed Column class

--- templates/entry.php
+++ templates/entry.php
@@ -32,10 +32,10 @@
         <?php $this->insert('translated_textarea', ["name" => "text", "string" => $entry->getText()]) ?>
     </div>
     <div class="row">
-        <div class="one-half">
+        <div class="one-half columns">
             <input class="button-primary" value="Save Entry" type="submit" name="entry_submit">
         </div>
-        <div class="one-half">
+        <div class="one-half columns">
             <input class="button-primary button-red" value="Delete Entry" type="submit" name="entry_delete"
                    onClick="return confirm('Are you sure you want to delete this Entry?')">
         </div>
